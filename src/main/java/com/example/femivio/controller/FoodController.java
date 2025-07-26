c3d1de9c971aebb2dd178798a4ef9e04550caedd
femivio <femivio2025@gmail.com>
2025-07-26 11:08:32 +0530

    updated

---
+++ src/main/java/com/example/femivio/controller/FoodController.java
@@ -0,0 +1,23 @@
+package com.example.femivio.controller;
+
+import com.example.femivio.Repository.FoodRepository;
+import com.example.femivio.model.Food;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/food")
+@CrossOrigin("*")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository foodRepository;
+
+    @GetMapping
+    public List<Food> getAllFood() {
+        return foodRepository.findAll();
+    }
+}
+
---
+++ src/main/java/com/example/femivio/model/Food.java
@@ -0,0 +1,65 @@
+package com.example.femivio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "Food")
+public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String imageUrl;
+    private double price;
+
+    // Constructors
+    public Food() {}
+
+    public Food(Long id, String name, String imageUrl, double price) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+    // Getters & Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
