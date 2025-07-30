b61a27ab4d478510424a43d2d79caa4fd1f6dccf
femivio <femivio2025@gmail.com>
2025-07-30 09:39:52 +0530

    Create FoodService.java

---
+++ src/main/java/com/example/femivio/Service/FoodService.java
@@ -0,0 +1,19 @@
+package com.example.femivio.service;
+
+import com.example.femivio.model.Food;
+import com.example.femivio.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FoodService {
+
+    @Autowired
+    private FoodRepository foodRepository;
+
+    public List<Food> getAllFoods() {
+        return foodRepository.findAll();
+    }
+}
