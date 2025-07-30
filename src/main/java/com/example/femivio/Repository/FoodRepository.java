f82a5af487650bc81e28b3751a7d902eb3831ac7
femivio <femivio2025@gmail.com>
2025-07-30 09:43:31 +0530

    Update FoodRepository.java

--- src/main/java/com/example/femivio/Repository/FoodRepository.java
+++ src/main/java/com/example/femivio/Repository/FoodRepository.java
@@ -1,7 +1,9 @@
-package com.example.femivio.Repository;
+package com.example.femivio.repository;
 
 import com.example.femivio.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
 }
