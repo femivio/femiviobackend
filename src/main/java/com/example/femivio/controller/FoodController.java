b9eceb3dd2364f06eea1ceadc7bf0447212209e3
femivio <femivio2025@gmail.com>
2025-07-30 09:47:01 +0530

    Update FoodController.java

--- src/main/java/com/example/femivio/controller/FoodController.java
+++ src/main/java/com/example/femivio/controller/FoodController.java
@@ -1,7 +1,7 @@
 package com.example.femivio.controller;
 
-import com.example.femivio.Repository.FoodRepository;
 import com.example.femivio.model.Food;
+import com.example.femivio.service.FoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,15 +9,24 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/food")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class FoodController {
 
     @Autowired
-    private FoodRepository foodRepository;
+    private FoodService foodService;
 
     @GetMapping
     public List<Food> getAllFood() {
-        return foodRepository.findAll();
+        return foodService.getAllFoods();
+    }
+
+    @PostMapping
+    public Food addFood(@RequestBody Food food) {
+        return foodService.addFood(food);
+    }
+
+    @GetMapping("/{id}")
+    public Food getFoodById(@PathVariable Long id) {
+        return foodService.getFoodById(id);
     }
 }
-
